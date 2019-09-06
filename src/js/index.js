6dba96abfaffefb3b1cd715736f32332c453a080
sa77irak <sa77irak@gmail.com>
2019-09-06 13:35:54 +0900

    added search feature

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+
+const state = {};
+
+const controlSearch = async () => {
+    const query = searchView.getInput();
+
+    if (query) {
+        state.search = new Search(query);
+
+        searchView.clearInput();
+        searchView.clearResults();
+
+        try {
+            await state.search.getResults();
+
+            searchView.renderResults(state.search.result);
+        } catch (err) {
+            alert('Something wrong with the search... ');
+        }
+    }
+}
+
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
