7a068083bbed1bbf49d8440d52440b2cf9fc3c2e
sa77irak <sa77irak@gmail.com>
2019-09-06 12:36:08 +0900

    added config.js gile

---
+++ src/js/config.js
@@ -0,0 +1,7 @@
+export const key = '556bded1c2d53238f6efb9453af04830';
+
+
+// https://www.food2fork.com/api/search
+// 4060c2889a4fd3a70ca5c876c19abcf6
+// https://www.food2fork.com/api/search?key=4060c2889a4fd3a70ca5c876c19abcf6&q=pizza
+
