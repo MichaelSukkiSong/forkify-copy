a21dc75b3eeec8dd89a0882d1e0cac530a6f3d9c
sa77irak <sa77irak@gmail.com>
2019-09-07 12:09:43 +0900

    updating recipe servings

--- src/js/index.js
+++ src/js/index.js
@@ -65,7 +65,7 @@ const controlRecipe = async () => {
             recipeView.renderRecipe(
                 state.recipe,
             );
-            
+
         } catch (err) {
             alert('Error processing recipe!');
         }
@@ -75,3 +75,16 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+
+
+elements.recipe.addEventListener('click', e => {
+   if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+       if (state.recipe.servings > 1) {
+           state.recipe.updateServings('dec');
+           recipeView.updateServingsIngredients(state.recipe);
+       }
+   } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+       state.recipe.updateServings('inc');
+       recipeView.updateServingsIngredients(state.recipe);
+   }
+});
